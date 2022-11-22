8b91d7cb292ef0d38c577240b77f116fc0f9d83a
Angel Daniel <daniel_70708@hotmail.com>
2022-11-21 18:43:56 -0600

    Se agrego el splashscreen y el logo de la apliacion

---
+++ app/src/main/java/com/example/audiolibros/SplashScreen.kt
@@ -0,0 +1,20 @@
+package com.example.audiolibros
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+
+class SplashScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        val splashScreen = installSplashScreen()
+
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        splashScreen.setKeepOnScreenCondition{true}
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
+        finish()
+    }
+}
