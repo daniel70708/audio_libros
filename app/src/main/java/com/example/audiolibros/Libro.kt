7c3bf810ec2397dac0789a1afce53575fb5aadef
Angel Daniel <daniel_70708@hotmail.com>
2022-12-13 22:19:22 -0600

    Se modifico la clase Libro y la clase LibroListraFragment, para poder incluir el ID del libro ya que nos servira para acceder a los audios del libro

--- app/src/main/java/com/example/audiolibros/Libro.kt
+++ app/src/main/java/com/example/audiolibros/Libro.kt
@@ -1,12 +1,14 @@
 package com.example.audiolibros
 
 data class Libro(
+    val ID: String,
     val titulo: String,
     val imagen: String,
     val resumen: String,
     val lanzamiento: String
     ){
     constructor(): this(
+         "",
         "",
         "",
         "",
--- app/src/main/java/com/example/audiolibros/LibroListaFragment.kt
+++ app/src/main/java/com/example/audiolibros/LibroListaFragment.kt
@@ -22,7 +22,7 @@ import io.grpc.util.TransmitStatusRuntimeExceptionInterceptor.instance
 class LibroListaFragment : Fragment() {
 
     private lateinit var recyclerView: RecyclerView
-    private lateinit var librosLista:ArrayList<Libro>
+    private lateinit var listaLibros: ArrayList<Libro>
     private lateinit var adaptador: AdaptadorLibros
     private lateinit var linearCargando: LinearLayout
 
@@ -40,50 +40,77 @@ class LibroListaFragment : Fragment() {
 
         linearCargando = vista.findViewById(R.id.linearCargando)
         recyclerView = vista.findViewById(R.id.recyclerView)
-        librosLista = arrayListOf()
-        adaptador = AdaptadorLibros(this, librosLista)
+        //Definimos el arreglo que vamos a llenar cuando obtengamos los objetos de Firebase
+        listaLibros = arrayListOf()
+        //Definimos que va a cargar los datos del arreglo librosLista
+        adaptador = AdaptadorLibros(this, listaLibros)
+        //El recyclerview mostrará dos libros de manera vertical
         recyclerView.layoutManager = GridLayoutManager(requireContext(), 2)
         recyclerView.adapter = adaptador
-        obtenerDatos()
+        obtenerLibros()
 
         return vista
     }
 
-    private fun obtenerDatos(){
+    /**Función que obtiene todos los datos (libros) que se encuentran en Firebase y que le pasamos al adaptadorLibros para que
+     * rellene el recyclerview con la portada y el nombre de cada libro. Además de implementar la funcionalidad de que va hacer
+     * cuando el usuario de click sobre item del recyclerview*/
+    private fun obtenerLibros(){
+        //Obtenemos todos los datos de la colección Libros
         FirebaseFirestore.getInstance().collection("Libros")
             .get()
             .addOnSuccessListener { documents ->
+                //Creamos la animacion de que está cargando los libros
+                linearCargando.visibility = View.INVISIBLE
+
+                //Recorremos todos los datos rellenenado el arreglo librosLista
                 for (document in documents){
+                    //Obtenemos cada objeto y lo pasamos a una variable llamada libro
                     val libro = document.toObject(Libro::class.java)
-                    librosLista.add(libro)
+                    listaLibros.add(libro)
                 }
+                //Cuando tenemos el arreglo de libros completo, ejecutamos el adaptador para que rellene nuestro recyclerview
                 recyclerView.adapter = adaptador
+                //Mostramos el recyclerview, pasando por detras la animación de cargar libros
+                recyclerView.visibility = View.VISIBLE
+
+                //Cunado el usuario de click en un libro mandamos a llamar a la función mostrarDetalleLibro y le pasamos objeto que se selecciono
                 adaptador.setOnClickListener(object : AdaptadorLibros.onItemClickListener{
                     override fun onItemClick(position: Int) {
-                        val bundle = bundleOf("Titulo" to librosLista[position].titulo, "Imagen" to librosLista[position].imagen)
-                        val fragmento = LibroDetalleFragment()
-                        fragmento.arguments = bundle
-
-                        parentFragmentManager.commit {
-                            setCustomAnimations(
-                                R.anim.slide_in,
-                                R.anim.fade_out,
-                                R.anim.fade_in,
-                                R.anim.slide_out
-                            )
-                            replace(R.id.contenedor_Fragment, fragmento)
-                            setReorderingAllowed(true)
-                            addToBackStack(null)
-                        }
+                        mostrarDetalleLibro(listaLibros[position])
                     }
                 })
 
-                linearCargando.visibility = View.INVISIBLE
-                recyclerView.visibility = View.VISIBLE
-
             }
             .addOnFailureListener {
-                Toast.makeText(context,"Error al obtener datos", Toast.LENGTH_LONG).show()
+                Toast.makeText(context,"Error al obtener datos: " +it, Toast.LENGTH_LONG).show()
             }
+    }
+
+    /**Función que muestra el fragmento libro_detalle que recibe como parámetro el objeto Libro seleccionado por el
+     * usuario y le envia esos datos a la  siguiente vista (reproductor del audiolibro)*/
+    fun mostrarDetalleLibro(libro: Libro){
+        //Le enviamos los datos a traves de un bundle
+        val bundle = bundleOf("ID" to libro.ID,
+            "Titulo" to libro.titulo,
+            "Imagen" to libro.imagen,
+            "Resumen" to libro.resumen,
+            "Lanzamiento" to libro.lanzamiento)
+
+        val fragmentoDetalle = LibroDetalleFragment()
+        fragmentoDetalle.arguments = bundle
+
+        //Realizamos la transaccion al fragment LibroDetalle
+        parentFragmentManager.commit {
+            setCustomAnimations(
+                R.anim.slide_in, //Animación de deslizamiento (derecha a izquierda) que muestra la siguiente vista
+                R.anim.fade_out, //Animación para la salida para este fragmento (alpha)
+                R.anim.fade_in, //Animación para regresar (deslizamiento de izquierda a derechz) cuando el usuario da click en regresar
+                R.anim.slide_out //Animación para desaparecer (alpha) el fragmento de detalle libro
+            )
+            replace(R.id.contenedor_Fragment, fragmentoDetalle)
+            setReorderingAllowed(true)
+            addToBackStack(null)
+        }
     }
 }
